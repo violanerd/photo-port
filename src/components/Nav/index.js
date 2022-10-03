5afa95d8e855b8bcb1b9e625ee98c01f8fb077b3
violanerd <madalyne.cross@gmail.com>
2022-10-03 11:45:24 -0500

    navbar on about

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 function Nav() {
+    const categories = [
+        {
+          name: "commercial",
+          description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+          name: "landscape",
+          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+      ];
+    function categorySelected(name){
+        console.log(`${name} clicked`)
+    }
     return(
         <header>
             <h2><a href="/">
@@ -15,6 +31,11 @@ function Nav() {
                     <li>
                         <span>Contact</span>
                     </li>
+                    {categories.map((category) => (
+                        <li className='mx-1' key={category.name}>
+                            <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
